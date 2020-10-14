d9a9878de852059af94137147eba03d4a6586b8f
SoSIE <sosie@sos-productions.com>
2020-10-14 22:39:41 +0200

    2.0.0

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@
 * Block position tracking plugin
 *
 * @Note this has to be triggered after await editor.isReady
-* @author sos-productions.com
+* @author sosie / sos-productions.com
 * @see remark in issue https://github.com/codex-team/editor.js/issues/1258
-* @version 2.0
+* @version 3.0
 * @history
 *    1.0 (05.10.2020) - Initial version from SoSIE
 *    2.0 (09.10.2020) - Refactoring init(editor) , add/remove BlocksPositionListeners works
+*    3.0 (14.10.2020) - initCaretManager added os saving, restoring caret position for injections works
 * @property {Object} editor - Editor.js API 
 **/
 
@@ -89,16 +90,27 @@ class BlockPlugin {
      * Extends editor with GetBlockNodeList
      * 
      *@description Hook to get Node Block list
-     *@internal This may surely be done less 'hookely'...from editor instance instead.
-     * Let say with nodejs, accessing to js editor dom is such a bunker 
-     * so I even spent a second trying to do so. If you know a cleaner 
-     * and faster way please inform me. - SoSIE
+     *@internal This may surely be done less 'hookely'...from editor instance instead like
+     * 
+     *  initGetBlockNodeList(editor) {
+     * 
+     *   editor.getBlocksNodeList = function() {
+     *       let blocks=[], b, bMax=editor.blocks.getBlocksCount();
+     *       for (b = 0; b < bMax ; b ++) {
+     *           blocks.push(editor.blocks.getBlockByIndex(b));
+     *       }
+     *       return blocks;
+     *   }
+     *  
+     *  } BUT IT DOES NOT WORK with addEventListener
+     * 
+     * If you know a cleaner and faster way please inform me. - SoSIE
      *@note editor.getBlocksNodeList function will be available
      *@param  {Object} editor - Editor.js API
      ***/ 
     initGetBlockNodeList(editor) {
         
-            editor.getBlocksNodeList = function() {
+        editor.getBlocksNodeList = function() {
         
             function getRedactor() {
                     //console.log(document.getElementsByClassName("codex-editor__redactor")); //Live, HTMLCollecton will not work
@@ -113,20 +125,35 @@ class BlockPlugin {
     }
     
     
+  /**
+    * Determines the position (index+1) of the given Block DOMElement
+    * 
+    * @note return editor.blocks.getCurrentBlockIndex(); + 1 only if block is current selected,
+    * Of course we could have mad a traversal of blocks to find which one matches block
+    * @param {HTMLDivElement} block
+    * @return {number} 
+    */
     getPosition(block) {
+      
+        // Compensates editor.blocks.array() as not available in BlockAPI
         var blocks = this.editor.getBlocksNodeList();
-        var p,n=Array.prototype.indexOf.call(blocks, block); 
-        //Do your stuff!
+        
+        // Compensates editor.blocks.indexOf(block: Block) as not available in BlockAPI
+        var p,n=Array.prototype.indexOf.call(blocks, block);
+        
         if(n==undefined) {
             p='?';
         }else {   
-            
             p=n+1;
         }
-        console.log("getPosition",{fromN: n , gives: p});
         return p;
     }
     
+    /**
+     * Install Listeners on each block to determine the position on which block has been clicked and track changes add, remove and updates  
+     * 
+    *@param  {Object} editor - Editor.js API
+    */
     initBlocksPositionListeners(editor) {
         
         var _this=this;
@@ -165,6 +192,10 @@ class BlockPlugin {
             } else {
                 blockSelection.end=end;
             }
+            
+            let caret=_this.getSelectionCharacterOffsetWithin(e.target); 
+            blockSelection.in=caret.in;
+            blockSelection.out=caret.out; 
             console.log(blockSelection);
             //refreshCopyBtn();
         }
@@ -243,8 +274,10 @@ class BlockPlugin {
     /**
      * Extends editor with Block Clipboard facility
      * 
+     *@author https://ourcodeworld.com/articles/read/282/how-to-get-the-current-cursor-position-and-selection-within-a-text-input-or-textarea-in-javascript
      *@description Add Clipboad support to determine if we are in collapsed mode, ie meaning if a block range is select, start and end matches.    
      *@note editor.clipboard.blockSelection will provide the last block range selected position {start:'?',end:'?'}
+     *@param  {Object} editor - Editor.js API
      **/
     initClipboard(editor) {
             
@@ -281,7 +314,8 @@ class BlockPlugin {
      /**
       * Statut Position Panel primitives
       * 
-      *@note SoSIE menu bar specific 
+      * @note SoSIE menu bar specific 
+      * @param  {Object} editor - Editor.js API
       **/   
     initBlockStatutPositionPanel(editor) {
     
@@ -309,23 +343,141 @@ class BlockPlugin {
         //Used by goto position function
         this.initScrollToBlock(editor);
     }
+    
+    /**
+    * Plugin extra caret position helpers and injector 
+    *    
+    * @param  {Object} editor - Editor.js API
+    **/
+    initCaretManager(editor) {
+      
+        // Move caret to a specific point in a DOM element
+        // Works for ContentEditable
+        // https://stackoverflow.com/questions/36869503/set-caret-position-in-contenteditable-div-that-has-children
+        function setCaretPosition(el, pos){
+
+            // Loop through all child nodes
+            for(var node of el.childNodes){
+                if(node.nodeType == 3){ // we have a text node
+                    if(node.length >= pos){
+                        // finally add our range
+                        var range = document.createRange(),
+                            sel = window.getSelection();
+                        range.setStart(node,pos);
+                        range.collapse(true);
+                        sel.removeAllRanges();
+                        sel.addRange(range);
+                        return -1; // we are done
+                    }else{
+                        pos -= node.length;
+                    }
+                }else{
+                    pos = setCaretPosition(node,pos);
+                    if(pos == -1){
+                        return -1; // no need to finish the for loop
+                    }
+                }
+            }
+            return pos; // needed because of recursion stuff
+        }
+        
+        /**
+        * Set caret position within the block of the given index
+        * @param {number} index - the block index 
+        * @param {number} caret - the caret position
+        **/
+        this.setCaretPosition = function (index, caret) {
+            var blocks=editor.getBlocksNodeList();
+            var blockTarget=blocks[index];
+            setCaretPosition(blockTarget, caret);
+        }
+        
+         /**
+        * Return an object with the selection range or cursor position (if both have the same value)
+        * 
+        * @param {DOMElement} el A dom of a ContentEditable element
+        * @see https://stackoverflow.com/questions/4811822/get-a-ranges-start-and-end-offsets-relative-to-its-parent-container/4812022#4812022
+        * @return {Object} reference Object with 2 properties (start and end) with the identifier of the location of the cursor and selected text. (as {in: , out:}) 
+        **/
+         this.getSelectionCharacterOffsetWithin = function (element) {
+            var start = 0;
+            var end = 0;
+            var doc = element.ownerDocument || element.document;
+            var win = doc.defaultView || doc.parentWindow;
+            var sel;
+            if (typeof win.getSelection != "undefined") {
+                sel = win.getSelection();
+                if (sel.rangeCount > 0) {
+                    var range = win.getSelection().getRangeAt(0);
+                    var preCaretRange = range.cloneRange();
+                    preCaretRange.selectNodeContents(element);
+                    preCaretRange.setEnd(range.startContainer, range.startOffset);
+                    start = preCaretRange.toString().length;
+                    preCaretRange.setEnd(range.endContainer, range.endOffset);
+                    end = preCaretRange.toString().length;
+                }
+            } else if ( (sel = doc.selection) && sel.type != "Control") {
+                var textRange = sel.createRange();
+                var preCaretTextRange = doc.body.createTextRange();
+                preCaretTextRange.moveToElementText(element);
+                preCaretTextRange.setEndPoint("EndToStart", textRange);
+                start = preCaretTextRange.text.length;
+                preCaretTextRange.setEndPoint("EndToEnd", textRange);
+                end = preCaretTextRange.text.length;
+            }
+            return { in: start, out: end };
+        }
+        
+        /**
+        * Helper to inject the Embed block inline at the start on selection
+        * 
+        * @note injection ie in selection place, no need to hiligh some chars to make inline tools appears
+        * @param {Element} el the element to inject
+        **/
+        this.inlineInjector = function (el) {
+  
+          var sel, range;
+          if (window.getSelection && (sel = window.getSelection()).rangeCount) {
+              range = sel.getRangeAt(0);
+              range.collapse(true);
+              
+              range.insertNode(el);
+
+              // Move the caret immediately after the inserted toolBlock
+              range.setStartAfter(el);
+              range.collapse(true);
+              sel.removeAllRanges();
+              sel.addRange(range);
+              
+              console.log('Block now injected inline');
+              
+          } else {
+            alert('Specify first a position with the caret by clicking inside a block');
+          }
+          
+        }
+    }
+      
         
     /* Initialisation entry point */    
     init(editor) {
         
         this.editor=editor;
         
-        //Add Clipboad support to determine if we are in collapsed mode, ie meaning if a block range is select, start and end matches.
+        // Add Clipboad support to determine if we are in collapsed mode, ie meaning if a block range is select, start and end matches.
         this.initClipboard(editor);
         
-        //Hook to get Node Block list
+        // Hook to get Node Block list
         this.initGetBlockNodeList(editor);
         
         // SoSIE menu bar specific 
         this.initBlockStatutPositionPanel(editor);
     
-        //Listeners on each block to determine the position on which block has been clicked and track changes add, remove and updates 
+        // Listeners on each block to determine the position on which block has been clicked and track changes add, remove and updates 
         this.initBlocksPositionListeners(editor);
+        
+        // primitives to set to a position /get caret from selection
+        this.initCaretManager(editor);
     
         console.info('Block plugin initialized');
     }
